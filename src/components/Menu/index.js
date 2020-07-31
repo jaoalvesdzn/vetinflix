06d67a29976f9248de4060914b685aa32c17b968
João Alves <joaoe2323@gmail.com>
2020-07-31 03:15:50 -0300

    terminado o carrosel

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
-//import ButtonLink from './components/ButtonLink';
+// import ButtonLink from './components/ButtonLink';
 
 function Menu() {
     return (
         <nav className="Menu">
             <Link to="/">
-                <img className="Logo" src={Logo} alt="Logo AluraFlix" />
+                <img className="Logo" src={Logo} alt="AluraFlix logo" />
             </Link>
 
-            <Button as={Link} className="ButtonLink" to='/cadastro/video'>
-                Novo Vídeo
-            </Button>
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
+                Novo vídeo
+      </Button>
         </nav>
     );
 }
